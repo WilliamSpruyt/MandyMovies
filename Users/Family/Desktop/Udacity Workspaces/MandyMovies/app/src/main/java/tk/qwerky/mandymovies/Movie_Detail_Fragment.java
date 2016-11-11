347d95486f2e04c068f1cdcb1d05802260966ce7
William Spruyt <williamspruyt@hotmail.com>
2016-11-11 15:27:26 +0000

    Initial commit

---
+++ Users/Family/Desktop/Udacity Workspaces/MandyMovies/app/src/main/java/tk/qwerky/mandymovies/Movie_Detail_Fragment.java
@@ -0,0 +1,29 @@
+package tk.qwerky.mandymovies;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Movie_Detail_Fragment extends Fragment {
+
+
+    public Movie_Detail_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie__detail_, container, false);
+    }
+
+}
